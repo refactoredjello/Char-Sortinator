51e9a4703b66423c95967c53e8a496f50decfaad
refactoredjello <gil.mandler@gmail.com>
2018-01-29 19:38:25 -0500

    Adds respoonse to the dom

--- client/main.js
+++ client/main.js
@@ -2,6 +2,12 @@ var getValue = function() {
   var str = document.getElementById('inputString').value
   return str
 }
+var addResult = function(str) {
+  var newDiv = document.createElement('div')
+  var newSortedString = document.createTextNode(str)
+  newDiv.appendChild(newSortedString)
+  document.body.appendChild(newDiv)
+}
 
 var postData = function(){
   var stringToSend = getValue()
@@ -12,6 +18,9 @@ var postData = function(){
     body: JSON.stringify(payload),
     headers: new Headers({'Content-Type': 'application/json'})
   })
+  .then(function(resp) { return resp.json() })
+  .catch(function(error) {console.log('Error')})
+  .then(function(resp) {addResult(resp.sortedString)})
 }
 
 var submit = document.getElementById('submit')
--- server/sortinator.js
+++ server/sortinator.js
@@ -1,7 +1,6 @@
 const sortString = (str) => {
   let strArr = str.split('')
   strArr.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0))
-  console.log(strArr.join(''))
   return strArr.join('')
 }
 
